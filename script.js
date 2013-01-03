1083ece57760f46ebe4d9a87cb0fa12ee5835a70
aioutecism <aioutecism@gmail.com>
2013-01-04 08:54:16 +0900

    phase added

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-console.log('aioute');
+var size = {
+	w: 465,
+	h: 465
+};
 
-var canvas = document.getElementById('canvas');
-canvas.innerText = '(๑╯ω╰๑)';
+paper.install(window);
+window.onload = function() {
+    paper.setup('canvas');
+
+    var path = new Path.Rectangle([0, 0], [size.w, size.h]);
+    path.strokeWidth = 20;
+    path.strokeColor = '#FFFFFF';
+
+    var musicPhase = 0.43;
+
+    view.onFrame = function(e) {
+    	path.strokeColor.blue = Math.abs((e.time / musicPhase) % 1 - 0.5) * 2;
+    	path.strokeColor.green = Math.abs((e.time / musicPhase) % 1 - 0.5) + 0.5;
+    };	
+}
