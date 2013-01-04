94189a6599c65a175e6377113221d8cb011e8da9
aioutecism <aioutecism@gmail.com>
2013-01-04 19:47:28 +0900

    working...

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-var size = {
-	w: 465,
-	h: 465
+// add
+// subtract
+// multiply
+// divide
+// modulo
+// equals
+var emoji = {
+    0: '',
+    1: '',
+    2: '',
+    crash: '',
 };
 
 paper.install(window);
 window.onload = function() {
     paper.setup('canvas');
+    var canvasSize = new Size(465, 465);
 
-    var path = new Path.Rectangle([0, 0], [size.w, size.h]);
-    path.strokeWidth = 20;
-    path.strokeColor = '#FFFFFF';
+    var phaseRectangle = new Path.Rectangle([0, 0], [canvasSize.width, canvasSize.height]);
+    phaseRectangle.strokeWidth = 20;
+    phaseRectangle.strokeColor = '#FFFFFF';
 
     var musicPhase = 0.43;
 
     view.onFrame = function(e) {
-    	path.strokeColor.blue = Math.abs((e.time / musicPhase) % 1 - 0.5) * 2;
-    	path.strokeColor.green = Math.abs((e.time / musicPhase) % 1 - 0.5) + 0.5;
+    	phaseRectangle.strokeColor.blue = Math.abs((e.time / musicPhase) % 1 - 0.5) * 2;
+    	phaseRectangle.strokeColor.green = Math.abs((e.time / musicPhase) % 1 - 0.5) + 0.5;
     };	
-}
+};
